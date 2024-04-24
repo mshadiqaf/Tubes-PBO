6e7f1b387666e2df58a618dbf892d2808014f277
mshadiqaf <mshadiqaf@gmail.com>
2024-04-25 01:36:57 +0800

    Inisialisasi project utama

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module project.tubespbo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires net.synedra.validatorfx;
+    requires eu.hansolo.tilesfx;
+
+    opens project.tubespbo to javafx.fxml;
+    exports project.tubespbo;
+    exports project.tubespbo.Controllers;
+    opens project.tubespbo.Controllers to javafx.fxml;
+}
