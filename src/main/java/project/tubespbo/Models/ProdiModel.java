df003c95aa94cdbbc8ef745367bdd2c17f9c74fa
Kurooo23 <142067591+Kurooo23@users.noreply.github.com>
2024-06-01 18:30:13 +0800

    Update ProdiModel.java : nambah properti nomorProdi

--- src/main/java/project/tubespbo/Models/ProdiModel.java
+++ src/main/java/project/tubespbo/Models/ProdiModel.java
@@ -2,13 +2,24 @@ package project.tubespbo.Models;
 
 public class ProdiModel {
 
-    private String nama;
+    private final String nama, deskripsi;
+    private final int nomorProdi;
 
-    public ProdiModel(String nama) {
+    public ProdiModel(String nama, int nomorProdi, String deskripsi) {
         this.nama = nama;
+        this.deskripsi = deskripsi;
+        this.nomorProdi = nomorProdi;
     }
 
     public String getNama() {
         return nama;
+    }
+
+    public int getNomorProdi() {
+        return nomorProdi;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
     }
 }
