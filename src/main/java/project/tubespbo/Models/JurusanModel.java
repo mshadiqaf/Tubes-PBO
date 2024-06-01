96179e24e9b8857c97846a2fd7be1f8f41f64420
Kurooo23 <142067591+Kurooo23@users.noreply.github.com>
2024-06-01 18:29:14 +0800

    Update JurusanModel.java

--- src/main/java/project/tubespbo/Models/JurusanModel.java
+++ src/main/java/project/tubespbo/Models/JurusanModel.java
@@ -1,16 +1,17 @@
 package project.tubespbo.Models;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class JurusanModel {
 
     private final String nama;
+    private String deskripsi;
     private final List<ProdiModel> daftarProdi;
 
-    public JurusanModel(String nama) {
+    public JurusanModel(String nama, String deskripsi, List<ProdiModel> daftarProdi) {
         this.nama = nama;
-        this.daftarProdi = new ArrayList<>();
+        this.deskripsi = deskripsi;
+        this.daftarProdi = daftarProdi;
     }
 
     public void addStudyPrograms(ProdiModel prodi) {
@@ -19,6 +20,10 @@ public class JurusanModel {
 
     public String getNama() {
         return nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
     }
 
     public List<ProdiModel> getDaftarStudi() {
